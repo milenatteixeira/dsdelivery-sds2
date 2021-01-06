9ba74022ed3f0c23cc8dff3a32c6411f3a20ad87
Milena Teixeira <milenateixeira648@gmail.com>
2021-01-06 14:45:01 -0300

    Endpoint PUT order delivered configured.

--- backend/src/main/java/com/devsuperior/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdelivery/controllers/OrderController.java
@@ -2,12 +2,13 @@ package com.devsuperior.dsdelivery.controllers;
 
 import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -32,5 +33,11 @@ public class OrderController {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
