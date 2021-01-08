ba1c0bc04bd0858e1d11145060c77ed3fb66b03c
Milena Teixeira <milenateixeira648@gmail.com>
2021-01-08 00:41:35 -0300

    Homepage layout done.

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,30 @@
+import { ReactComponent } from "*.svg";
+import "./styles.css";
+import {ReactComponent as MainImage} from "./imagem.svg";
+
+function Home() {
+	return (
+		<div className="home-container">
+			<div className="home-content">
+				<div className="home-actions">
+					<h1 className="home-title">
+						Faça seu pedido
+						<br /> que entregamos
+						<br /> pra você!!!
+					</h1>
+					<h3 className="home-subtitle">
+						Escolha seu pedido e em poucos minutos <br /> levaremos na sua porta
+					</h3>
+					<a href="orders" className="home-btn-order">
+						FAZER PEDIDO
+					</a>
+				</div>
+                <div className="home-img">
+                    <MainImage />
+                </div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
